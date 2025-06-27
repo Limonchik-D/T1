ea7beda0776f5bd6de680533f3b2c927796309bd
Давид Махвиладзе <DavLimon4@yandex.com>
2025-06-27 11:54:06 +0300

    Add files via upload

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var triangle1 = new Triangle<int>(3, 4, 5);
+                triangle1.PrintInfo();
+
+                var triangle2 = new Triangle<uint>(5, 12, 13);
+                triangle2.PrintInfo();
+
+                var triangle3 = new Triangle<double>(7.5, 8.2, 9.3);
+                triangle3.PrintInfo();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp7/Triangle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    public class Triangle<T>
+    {
+        private T a, b, c;
+
+        public T A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        public T B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+
+        public T C
+        {
+            get { return c; }
+            set { c = value; }
+        }
+
+        public Triangle(T a, T b, T c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Стороны: A={A}, B={B}, C={C}");
+            Console.WriteLine($"Площадь: {GetArea():F2}");
+        }
+
+        public double GetArea()
+        {
+            double x = Convert.ToDouble(A);
+            double y = Convert.ToDouble(B);
+            double z = Convert.ToDouble(C);
+
+            double s = (x + y + z) / 2;
+            return Math.Sqrt(s * (s - x) * (s - y) * (s - z));
+        }
+    }
+}
+/*
+ * int Name
+ * {
+ * get {return name;}
+ * set {name = value;}
+ * }
+ */
